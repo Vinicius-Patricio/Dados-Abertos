8657876c062f98381ada1d0ff2b9c4da93f3b49d
Vinicius Patricio <viniciuspatricio.33@gmail.com>
2025-07-27 21:05:37 -0300

    feat: adiciona filtros por nome, id, partido e uf na lista de deputados

--- backend/app/Http/Controllers/DeputadosController.php
+++ backend/app/Http/Controllers/DeputadosController.php
@@ -6,13 +6,74 @@ use Illuminate\Http\Request;
 
 class DeputadosController extends Controller
 {
-    public function buscarDados()
+    public function buscarDados(Request $request)
     {
-        $response = Http::get('https://dadosabertos.camara.leg.br/api/v2/deputados');
+        $pagina = $request->get('pagina', 1);
+        $itensPorPagina = $request->get('itens_por_pagina', 20);
+        
+        $nome = $request->get('nome');
+        $id = $request->get('id');
+        $partido = $request->get('partido');
+        $uf = $request->get('uf');
+        
+        $url = 'https://dadosabertos.camara.leg.br/api/v2/deputados';
+        $params = [
+            'ordem' => 'ASC',
+            'ordenarPor' => 'nome',
+            'pagina' => $pagina,
+            'itens' => $itensPorPagina
+        ];
+        
+        if ($nome) {
+            $params['nome'] = $nome;
+        }
+        if ($id) {
+            $params['id'] = $id;
+        }
+        if ($partido) {
+            $params['siglaPartido'] = $partido;
+        }
+        if ($uf) {
+            $params['siglaUf'] = $uf;
+        }
+        
+        $response = Http::get($url, $params);
+        
         if ($response->successful()) {
-            return response()->json($response->json());
+            $dados = $response->json();
+            
+            $dados['paginacao'] = [
+                'pagina_atual' => (int) $pagina,
+                'itens_por_pagina' => (int) $itensPorPagina,
+                'total_itens' => $dados['dados'] ? count($dados['dados']) : 0,
+                'tem_proxima_pagina' => count($dados['dados']) >= $itensPorPagina,
+                'total_deputados' => $dados['links'] ? $this->extrairTotalDeputados($dados['links']) : 0
+            ];
+
+            $dados['filtros'] = [
+                'nome' => $nome,
+                'id' => $id,
+                'partido' => $partido,
+                'uf' => $uf
+            ];
+            
+            return response()->json($dados);
         } else {
             return response()->json(['erro' => 'Falha ao buscar dados'], 500);
         }
+    }
+
+    private function extrairTotalDeputados($links)
+    {
+        foreach ($links as $link) {
+            if ($link['rel'] === 'last') {
+                $url = $link['href'];
+                if (preg_match('/pagina=(\d+)/', $url, $matches)) {
+                    $ultimaPagina = (int) $matches[1];
+                    return $ultimaPagina * 20;
+                }
+            }
+        }
+        return 0;
     }
 }
